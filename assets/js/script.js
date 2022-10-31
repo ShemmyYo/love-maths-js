df9a51d2409b1f651b561485149e0e7dc4f527c7
ShemmyYo <rabiliamemo@gmail.com>
2022-10-31 14:57:41 +0000

    add code to calculate 2 random numbers

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+     
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+/**
+ * The main "loop", called when thescript is first loaded 
+ * and after the user's answer has been processed 
+ */
+function runGame() {
+    // Creates 2 random number between 1 and 26
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestions() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}

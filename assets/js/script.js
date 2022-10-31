c3dc7a001ecac17f6043634b3059c1200b68cb12
ShemmyYo <rabiliamemo@gmail.com>
2022-10-31 18:20:32 +0000

    added favicon and tidyed up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,18 +10,27 @@ document.addEventListener("DOMContentLoaded", function() {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
             }
-        });
+        })
     }
 
-    runGame("addition");
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
 
-});
+    runGame("addition");
+})
 
 /**
  * The main game "loop", called when the script is first loaded 
  * and after the user's answer has been processed 
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+    
     // Creates 2 random number between 1 and 26
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
